1d16ab1091c73644448ea7903fb0c3ac01043300
Matthew Peveler <matt.peveler@gmail.com>
2019-04-11 16:41:14 -0400

    Use @cisl/logger if installed instead of console

--- index.js
+++ index.js
@@ -4,23 +4,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+let logger;
+try {
+  logger = require('@cisl/logger');
+}
+catch (e) {
+  logger = console;
+  logger.info = logger.log;
+}
+
 let config_file = path.resolve('cog.json');
 let config;
 if (!fs.existsSync(config_file)) {
-  console.error('Could not find cog.json file');
+  logger.error('Could not find cog.json file');
   process.exit(-1);
 }
 try {
   config = JSON.parse(fs.readFileSync(config_file, {encoding: 'ascii'}));
 }
 catch (e) {
-  console.error('Error: could not parse cog.json file');
-  console.error(e);
+  logger.error('Error: could not parse cog.json file');
+  logger.error(e);
   process.exit(-1);
 }
 
 if (!config.port) {
-  console.error('You must set a port in cog.json');
+  logger.error('You must set a port in cog.json');
   process.exit(-1);
 }
 
@@ -38,7 +47,7 @@ app.set('view engine', 'ejs');
 app.express_listen = app.listen;
 app.listen = () => {
   app.express_listen(app.get('port'), () => {
-    console.log(`Express server listening on port ${config.port}`);
+    logger.log(`Express server listening on port ${config.port}`);
   });
 };
 
